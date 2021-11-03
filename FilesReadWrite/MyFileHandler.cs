f28966182e2648ffa4166c5847ba317777f20795
Omar-Slik <92442894+Omar-Slik@users.noreply.github.com>
2021-11-03 19:28:59 +0100

    commit

---
+++ FilesReadWrite/MyFileHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesReadWrite
+{
+    internal class MyFileHandler
+    {
+        public void ReadAllContent()
+        {
+            string fileContent = File.ReadAllText("testfile1.txt");
+            Console.WriteLine(fileContent);
+        }
+        public void WriteContentToFile(string input)
+        {
+            File.WriteAllText( "testfile2.txt", input);
+
+        }
+        public void UpperCaseContent(string from,string to)
+        {
+            string fileContent=File.ReadAllText(from);
+            File.WriteAllText(to, fileContent.ToUpper());
+            string filecontent = File.ReadAllText(to);
+            Console.WriteLine(filecontent);
+        }
+        ////public void ReadLatinEncodedContent()
+        ////{
+        ////    string fileContent = File.ReadAllText("latin1.txt");
+        ////    Console.WriteLine(fileContent);
+        ////}
+    }
+}
---
+++ FilesReadWrite/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace FilesReadWrite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyFileHandler nyMyFileHandler = new MyFileHandler();
+            //nyMyFileHandler.ReadAllContent();
+            //nyMyFileHandler.WriteContentToFile("Hejsan!");
+            //nyMyFileHandler.UpperCaseContent("testfile1.txt", "testfile3.txt");
+            //nyMyFileHandler.ReadLatinEncodedContent();
+            NoteBook noteBook = new NoteBook();
+
+            while (true)
+            {
+                Console.Clear();
+                string[] allnotes = noteBook.GetAllNotes();
+                Console.WriteLine("persneter notes:");
+                foreach (var item in allnotes)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine("------------------------");
+                Console.WriteLine("Write a note: ");
+                string input = Console.ReadLine();
+                if (input == "")
+                {
+                    Console.WriteLine("Wrong input!!");
+                }
+                noteBook.TakeNote(input);
+            }
+        }
+    }
+}
